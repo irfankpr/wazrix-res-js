9871b9369a2a567c9f408c26fd0ea11c1594fecb
irfan <105389506+irfankpr@users.noreply.github.com>
2022-08-01 11:04:04 +0530

    more validation added

--- waz.js
+++ waz.js
@@ -39,6 +39,64 @@ function isname() {
     }
 }
 
+function isMail() {
+    var x = document.getElementById("mail").value;
+    var len = x.length;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x) || len == 0) {
+        document.getElementById("Malrt").innerHTML = "";
+    } else {
+        document.getElementById("Malrt").innerHTML = "Enter a valid e mail";
+    }
+}
+
+function isTen() {
+    var x = document.getElementById("phn").value;
+    var len = x.length;
+    if (len == 0) {
+        document.getElementById("phalrt").innerHTML = "";
+    } else if (len < 10 || len > 10) {
+        document.getElementById("phalrt").innerHTML = "Contact number must be 10 digit.";
+    } else {
+        document.getElementById("phalrt").innerHTML = "";
+    }
+}
+
+function Sopt1(){
+    var len= document.getElementById("opt1").value.length;
+    if( len == 0 ){
+            document.getElementById("Aopt1").innerHTML= "Language cannot be empty";
+    }else{
+        document.getElementById("Aopt1").innerHTML= "";
+    }
+
+}
+
+function Sopt2(){
+    var len= document.getElementById("opt2").value.length;
+    if( len == 0 ){
+            document.getElementById("Aopt2").innerHTML= "Reason cannot be empty";
+    }else{
+        document.getElementById("Aopt2").innerHTML= "";
+    }
+
+}
+function  isInput(){
+    var len1= document.getElementById("opt1").value.length;
+    var len2= document.getElementById("opt2").value.length;
+    var len3= document.getElementById("Name").value.length;
+    if( len1 != 0 ){
+        document.getElementById("Aopt1").innerHTML= "";
+    }
+    if( len2 != 0){
+        document.getElementById("Aopt2").innerHTML= "";
+    }
+    if(len3 == 0){
+        document.getElementById("Nalrt").innerHTML = "Name cannot be empty"
+    }else{
+        document.getElementById("Nalrt").innerHTML = ""
+    }
+}
+
 function reg() {
     document.getElementById("cct").style.backgroundColor = "#f9de38";
     document.getElementById("cct").innerText = "CONTACT US";
@@ -48,5 +106,5 @@ function reg() {
     document.write("<h1>Thank you, We will contact you soon.</h1>");
     alert("We will connect you soon.");
     window.location.reload("if.html");
-    
+
 }
